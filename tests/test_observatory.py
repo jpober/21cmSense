7b64c0fb33f98ca66dcfa923f4af2d1a624f372c
Steven Murray <steven.murray@curtin.edu.au>
2019-11-14 11:41:26 -0700

    Observatory tests

---
+++ tests/test_observatory.py
@@ -0,0 +1,89 @@
+import pytest
+
+import numpy as np
+from astropy import units
+from py21cmsense import Observatory
+from py21cmsense.beam import GaussianBeam
+
+
+@pytest.fixture(scope="module")
+def bm():
+    return GaussianBeam(frequency=150.0, dish_size=14)
+
+
+def test_antpos(bm):
+    a = Observatory(antpos=np.zeros((10, 3)), beam=bm)
+    assert a.antpos.unit == units.m
+
+    assert np.all(a.baselines_metres == 0)
+
+    # If bad units given, should raise error.
+    with pytest.raises(units.UnitConversionError):
+        Observatory(antpos=np.zeros((10, 3)) * units.s, beam=bm)
+
+    # Need more than one antenna
+    with pytest.raises(AssertionError):
+        Observatory(antpos=np.zeros((1, 3)), beam=bm)
+
+
+def test_observatory_class(bm):
+    a = Observatory(antpos=np.zeros((3, 3)), beam=bm)
+    b = a.clone()
+    assert a == b
+
+
+def test_Trcv(bm):
+    a = Observatory(antpos=np.zeros((3, 3)), beam=bm, Trcv=10)
+    assert a.Trcv.unit == units.mK
+
+
+def test_observatory(bm):
+    a = Observatory(antpos=np.zeros((3, 3)), beam=bm)
+    assert a.frequency == bm.frequency
+    assert a.baselines_metres.shape == (3, 3, 3)
+    assert (
+        a.baselines_metres * a.metres_to_wavelengths
+    ).unit == units.dimensionless_unscaled
+    assert a.baseline_lengths.shape == (3, 3)
+    assert np.all(a.baseline_lengths == 0)
+
+    b = Observatory(antpos=np.array([[0, 0, 0], [1, 0, 0], [3, 0, 0]]), beam=bm)
+    assert units.isclose(
+        b.shortest_baseline / b.metres_to_wavelengths, 1 * units.m, rtol=1e-3
+    )
+    assert units.isclose(
+        b.longest_baseline / b.metres_to_wavelengths, 3 * units.m, rtol=1e-3
+    )
+    assert b.observation_duration < 1 * units.day
+    assert len(b.get_redundant_baselines()) == 6  # including swapped ones
+    with pytest.raises(AssertionError):
+        b.time_offsets_from_obs_int_time(b.observation_duration)
+
+    assert len(b.time_offsets_from_obs_int_time(b.observation_duration / 1.05)) == 2
+    assert units.isclose(
+        b.longest_used_baseline() / b.metres_to_wavelengths, 3 * units.m, rtol=1e-3
+    )
+
+
+def test_beamgridder():
+    uvgrid = np.zeros((11, 11))
+    Observatory.beamgridder(0, 0, uvgrid, 1)
+    print(uvgrid)
+    assert uvgrid[5, 5] == 1
+
+    Observatory.beamgridder(5, 5, uvgrid, 1)
+    print(uvgrid)
+    assert uvgrid[-1, -1] == 1
+
+    Observatory.beamgridder(0.5, 0.5, uvgrid, 1)
+    assert uvgrid[6, 6] == 1
+
+    Observatory.beamgridder(-0.5, -0.5, uvgrid, 1)
+    assert uvgrid[4, 4] == 1
+
+
+@pytest.mark.skip
+def test_projected_baselines():
+    obs = Observatory()
+    assert obs.projected_baselines() == obs.baselines_metres
+    pass
