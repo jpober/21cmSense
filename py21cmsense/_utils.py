1237bb492fefaf3fd1804b47e73957545177841b
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2020-07-24 11:40:57 -0700

    allow find_nearest to take vector input

--- py21cmsense/_utils.py
+++ py21cmsense/_utils.py
@@ -71,7 +71,7 @@ def nonnegative(instance, att, x):
 
 def find_nearest(array, value):
     """Find closest value in `array` to `value`"""
-    idx = (np.abs(array - value)).argmin()
+    idx = np.abs(array.reshape(-1, 1) - value).argmin(0)
     return idx
 
 
