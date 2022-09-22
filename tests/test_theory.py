d3d03d9a35a6fab9d4f8b9e3722919b1f582aae4
Steven Murray <steven.g.murray@asu.edu>
2022-09-22 08:43:48 -0700

    test: add test of Legacy21cmFAST theory

--- tests/test_theory.py
+++ tests/test_theory.py
@@ -2,10 +2,10 @@ import pytest
 
 import numpy as np
 
-from py21cmsense.theory import EOS2021
+from py21cmsense.theory import EOS2021, Legacy21cmFAST
 
 
-def test_extrapolation():
+def test_eos_extrapolation():
     eos = EOS2021()
 
     with pytest.warns(
@@ -22,3 +22,21 @@ def test_extrapolation():
         UserWarning, match="Extrapolating beyond simulated redshift range"
     ):
         eos.delta_squared(50, np.array([0.1]))
+
+
+def test_legacy():
+    theory = Legacy21cmFAST()
+    assert theory.delta_squared(9.0, 1.0) == theory.delta_squared(10.0, 1.0)
+
+    with pytest.warns(UserWarning, match="Theory power corresponds to z=9.5, not z"):
+        theory.delta_squared(1.0, 1.0)
+
+    with pytest.warns(
+        UserWarning, match="Extrapolating above the simulated theoretical k"
+    ):
+        theory.delta_squared(15, np.array([0.1, 1e6]))
+
+    with pytest.warns(
+        UserWarning, match="Extrapolating below the simulated theoretical k"
+    ):
+        theory.delta_squared(15, np.array([0.0001, 0.1]))
